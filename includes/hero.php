cd074821521d6ce4a50f359d335c26483d18712d
Sven Berger <bergersvenoliver@gmail.com>
2026-03-08 12:52:47 +0100

    Test for Deploy

--- includes/hero.php
+++ includes/hero.php
@@ -1,10 +1,10 @@
     <header class="bg-warning py-5 align-items-center">
-      <div class="container">
-        <h1 class="mb-0">
-          <?php
-            $world = "Welt";
+        <div class="container">
+            <h1 class="mb-0">
+                <?php
+            $world = "Welt 2";
             echo "Hallo " . $world . "!";
           ?>
-        </h1>
-      </div>
+            </h1>
+        </div>
     </header>
